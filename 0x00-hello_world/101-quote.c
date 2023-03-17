cbb12d915ca226aad6265485a993e003612ae55b
VEGLOhouetchenouGABIN <houetchenougabinveglo>
2023-03-16 22:50:51 -0700

    ghbjn,

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,8 @@
 #include <unistd.h>
-
 int main(void)
 {
-    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    const size_t len = sizeof(msg) - 1;
-    ssize_t ret;
-
-    ret = write(STDERR_FILENO, msg, len);
-    return (ret == len) ? 1 : -1;
-}
+const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const size_t len = sizeof(msg) - 1;
+ssize_t ret;
+ret = write(STDERR_FILENO, msg, len);
+return (ret == len) ? 1 : -1;
