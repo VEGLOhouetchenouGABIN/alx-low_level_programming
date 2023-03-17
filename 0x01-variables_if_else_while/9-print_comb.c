a555ef2c1c497b0483faa2791fbc46a06e5d7b39
VEGLOhouetchenouGABIN <houetchenougabinveglo>
2023-03-17 11:32:19 -0700

    ghbjn

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: prints all possible combinations of single-digit numbers
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int num;
-for (num = 0; num <= 9; num++)
+ int i;
+for (i = 0; i < 10; i++)
 {
-int temp;
-for (temp = num; temp <= 9; temp++)
-{
-putchar(num + '0');
-if (temp != num)
-{
-putchar(temp + '0');
-}
-if (num < 8 || temp < 9)
+putchar(i + '0');
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
