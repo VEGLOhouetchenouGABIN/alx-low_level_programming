e64e58c5880cff88fc878479290c407f2fa0f7a9
VEGLOhouetchenouGABIN <houetchenougabinveglo>
2023-03-21 18:22:36 -0700

    ghjnk,

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints the alphabet in lowercase ten times followed by a newline
+ * print_alphabet_x10 - prints the alphabet in lowercase ten
+ * times followed by a newline
+ *
  */
 void print_alphabet_x10(void)
 {
