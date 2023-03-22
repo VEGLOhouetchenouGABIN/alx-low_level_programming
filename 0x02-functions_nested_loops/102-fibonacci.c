b6e9a5701416818ba4a3ae94e942dae168193fa2
VEGLOhouetchenouGABIN <houetchenougabinveglo>
2023-03-21 18:38:34 -0700

    hjk,

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main function
+ * count fibonacci
+ *
+ */
 int main(void)
 {
 int i, count = 50;
@@ -6,7 +11,7 @@ long int fib1 = 1, fib2 = 2, nextTerm;
 printf("%ld, %ld", fib1, fib2);
 for (i = 3; i <= count; i++)
 {
-nextTerm = fib1 + fib2;
+nextTerm = (fib1 + fib2);
 printf(", %ld", nextTerm);
 fib1 = fib2;
 fib2 = nextTerm;
