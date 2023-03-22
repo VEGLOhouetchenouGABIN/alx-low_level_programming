b38d15d775ece73e55f383c40a77e40ba946cba0
VEGLOhouetchenouGABIN <houetchenougabinveglo>
2023-03-21 19:11:33 -0700

    jk,l;

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,36 @@
 #include "main.h"
 /**
  * print_to_98 - prints all natural numbers from n to 98
- * @n: starting number
- *
- * Return: void
+ * @n: the starting number
  */
 void print_to_98(int n)
 {
 int i;
-if (n <= 98) {
-for (i = n; i < 98; i++) {
-printf("%d, ", i);
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
 }
-} else {
-for (i = n; i > 98; i--) {
-printf("%d, ", i);
 }
 }
-printf("98\n");
+else
+{
+for (i = n; i >= 98; i--)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
 }
