fe32db9af50e3712a6b96738e1fc5b09743457f0
VEGLOhouetchenouGABIN <houetchenougabinveglo>
2023-04-05 02:56:37 -0700

    My new commit for this repo

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
+
 int actual_prime(int n, int i);
+
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
@@ -8,10 +10,11 @@ int actual_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-        if (n <= 1)
-                return (0);
-        return (actual_prime(n, n - 1));
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
 }
+
 /**
  * actual_prime - calculates if a number is prime recursively
  * @n: number to evaluate
@@ -21,9 +24,9 @@ int is_prime_number(int n)
  */
 int actual_prime(int n, int i)
 {
-        if (i == 1)
-                return (1);
-        if (n % i == 0 && i > 0)
-                return (0);
-        return (actual_prime(n, i - 1));
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
 }
